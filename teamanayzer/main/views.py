75d09111a88c77c80d8f836bfb16f8aedfb97392
slimpotatoes <slimpotatoes@bitbucket.org>
2017-03-25 13:14:00 -0400

    Bla

--- teamanayzer/main/views.py
+++ teamanayzer/main/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.views import generic
 from django.urls import reverse
 
-from .forms import SummonerNameForm
+#from .forms import SummonerNameForm
 
 # Create your views here.
 
@@ -16,7 +16,7 @@ class FrontView(generic.TemplateView):
 
 def get_name(request):
     # if this is a POST request we need to process the form data
-    if request.method == 'POST':
+    '''if request.method == 'POST':
         # create a form instance and populate it with data from the request:
         form = SummonerNameForm(request.POST)
         # check whether it's valid:
@@ -28,6 +28,10 @@ def get_name(request):
 
     # if a GET (or any other method) we'll create a blank form
     else:
-        form = SummonerNameForm()
+        form = SummonerNameForm()'''
+    if request.method == 'GET':
+        url_full = request.get_full_path()
+        split = url_full.split("=")
+        summoner_name = split[-1]
 
-    return render(request, 'main.html', {'summonernameform': form})
+    return render(request, 'main.html')
